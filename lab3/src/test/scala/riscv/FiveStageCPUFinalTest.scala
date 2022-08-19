9c4791c5e94a4b68a7fa08c2146ca0980de648c6
OnewayLab <1878213955@qq.com>
2022-08-19 13:24:10 +0800

    lab3: add hazard tests

--- lab3/src/test/scala/riscv/FiveStageCPUFinalTest.scala
+++ lab3/src/test/scala/riscv/FiveStageCPUFinalTest.scala
@@ -17,12 +17,10 @@ package riscv
 import chisel3._
 import chiseltest._
 import org.scalatest.flatspec.AnyFlatSpec
-import peripheral.{InstructionROM, Memory, ROMLoader}
-import riscv.core.CPU
 
 
 class FiveStageCPUFinalTest extends AnyFlatSpec with ChiselScalatestTester {
-  behavior of "Five-stage Pipelined CPU with Short Branch Delay"
+  behavior of "Five-stage Pipelined CPU with Reduced Branch Delay"
   it should "calculate recursively fibonacci(10)" in {
     test(new TestTopModule("fibonacci.asmbin", ImplementationType.FiveStageFinal)).withAnnotations(TestAnnotations.annos) { c =>
       for (i <- 1 to 50) {
@@ -50,16 +48,26 @@ class FiveStageCPUFinalTest extends AnyFlatSpec with ChiselScalatestTester {
   }
   it should "store and load single byte" in {
     test(new TestTopModule("sb.asmbin", ImplementationType.FiveStageFinal)).withAnnotations(TestAnnotations.annos) { c =>
-      for (i <- 1 to 500) {
-        c.clock.step()
-        c.io.mem_debug_read_address.poke((i * 4).U) // Avoid timeout
-      }
+      c.clock.step(1000)
       c.io.regs_debug_read_address.poke(5.U)
       c.io.regs_debug_read_data.expect(0xDEADBEEFL.U)
       c.io.regs_debug_read_address.poke(6.U)
       c.io.regs_debug_read_data.expect(0xEF.U)
       c.io.regs_debug_read_address.poke(1.U)
       c.io.regs_debug_read_data.expect(0x15EF.U)
+    }
+  }
+  it should "solve data and control hazards" in {
+    test(new TestTopModule("hazard.asmbin", ImplementationType.FiveStageFinal)).withAnnotations(TestAnnotations.annos) { c =>
+      c.clock.step(1000)
+      c.io.regs_debug_read_address.poke(1.U)
+      c.io.regs_debug_read_data.expect(26.U)
+      c.io.mem_debug_read_address.poke(4.U)
+      c.clock.step()
+      c.io.mem_debug_read_data.expect(1.U)
+      c.io.mem_debug_read_address.poke(8.U)
+      c.clock.step()
+      c.io.mem_debug_read_data.expect(3.U)
     }
   }
 }
--- lab3/src/test/scala/riscv/ThreeStageCPUTest.scala
+++ lab3/src/test/scala/riscv/ThreeStageCPUTest.scala
@@ -48,16 +48,26 @@ class ThreeStageCPUTest extends AnyFlatSpec with ChiselScalatestTester {
   }
   it should "store and load single byte" in {
     test(new TestTopModule("sb.asmbin", ImplementationType.ThreeStage)).withAnnotations(TestAnnotations.annos) { c =>
-      for (i <- 1 to 500) {
-        c.clock.step()
-        c.io.mem_debug_read_address.poke((i * 4).U) // Avoid timeout
-      }
+      c.clock.step(1000)
       c.io.regs_debug_read_address.poke(5.U)
       c.io.regs_debug_read_data.expect(0xDEADBEEFL.U)
       c.io.regs_debug_read_address.poke(6.U)
       c.io.regs_debug_read_data.expect(0xEF.U)
       c.io.regs_debug_read_address.poke(1.U)
       c.io.regs_debug_read_data.expect(0x15EF.U)
+    }
+  }
+  it should "solve control hazards" in {
+    test(new TestTopModule("hazard.asmbin", ImplementationType.ThreeStage)).withAnnotations(TestAnnotations.annos) { c =>
+      c.clock.step(1000)
+      c.io.regs_debug_read_address.poke(1.U)
+      c.io.regs_debug_read_data.expect(26.U)
+      c.io.mem_debug_read_address.poke(4.U)
+      c.clock.step()
+      c.io.mem_debug_read_data.expect(1.U)
+      c.io.mem_debug_read_address.poke(8.U)
+      c.clock.step()
+      c.io.mem_debug_read_data.expect(3.U)
     }
   }
 }
